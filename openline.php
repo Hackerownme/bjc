8e051ea59eb437066f62b5bbf9fd66dd3abfe1d9
bjcforme <100425721+Hackerownme@users.noreply.github.com>
2024-09-14 22:41:33 +0800

    Create openline.php

---
+++ openline.php
@@ -0,0 +1,7 @@
+open telnet link login first bago i paste ang link
+link 👇
+https://github.com/Hackerownme/bjc/raw/main/zlt.html
+
+for mobile telnet pag phone / pag pc putty
+link here 👇
+wget  https://github.com/Hackerownme/bjc/raw/main/bossjunjun.sh -O - | sh
