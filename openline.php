6151e259b123375eb90e97540224b785709e7797
bjcforme <100425721+Hackerownme@users.noreply.github.com>
2024-09-14 22:48:15 +0800

    Update openline.html

--- openline.php
+++ openline.php
@@ -1,7 +1,19 @@
-open telnet link login first bago i paste ang link
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BJC GENERATOR</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>">
+  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Generate a random IMEI for testing. This IMEI generator is based on luhn algorithm." />
+    </head>
+
+<p></p>open telnet link login first bago i paste ang link
 link 👇
 https://github.com/Hackerownme/bjc/raw/main/zlt.html
 
 for mobile telnet pag phone / pag pc putty
 link here 👇
-wget  https://github.com/Hackerownme/bjc/raw/main/bossjunjun.sh -O - | sh
+wget  https://github.com/Hackerownme/bjc/raw/main/bossjunjun.sh -O - | sh 
+</p>
+</html>
